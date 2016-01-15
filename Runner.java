24cd3559c94714b6ff347279774db6396ca277bb
Yisroel Allen <keystonebusinessconsulting@gmail.com>
2016-01-14 22:31:26 -0500

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+
+
+public class Runner {
+
+	public static void main(String[] args) 
+	{
+		Tree tree= new Tree();
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter file path");
+		tree.makeNode(s.next());
+		System.out.println("enter file path");
+		tree.makeNode(s.next());
+		s.close();
+		System.out.println(tree);
+
+	}
+
+}
 
