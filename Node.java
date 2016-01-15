3f59bc0b34515864ce8d6ccadf5a16ae71a732f8
Yisroel Allen <keystonebusinessconsulting@gmail.com>
2016-01-14 22:29:08 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,1 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Node 
+{
+	String fileName;
+	Map<String,Node> subfiles= new HashMap<String,Node>();
+	
+	public Node()
+	{
+		fileName=null;
+	}
+	public Node(String s)
+	{
+		fileName=s;
+	}
+	public @Override String toString()
+	{
+		return fileName;
+	}
+}
 
